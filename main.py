0f6a4875e0206ac317b5d09f6f779d5a11a6da69
GrayJack <gr41.j4ck@gmail.com>
2023-08-03 11:22:33 -0300

    feat: Add function to check if point is inside contour

--- main.py
+++ main.py
@@ -27,6 +27,36 @@ def read_contour_file(file_path: str) -> pd.DataFrame:
     return pd.DataFrame(float_raw_lines, columns=["lat", "long"])
 
 
+def is_inside_contour(edges, lat: float, long: float) -> bool:
+    # Checking if the point is inside the contour by casting a ray at one side from the
+    # `(lat, long)` point and counting if number of times the line crosses the polygon.
+    # If it's odd, the point is inside, otherwise it is outside.
+    #
+    # The case is always true independent of the ray direction. So for simplicity, we
+    # are always casting a ray to the left of the point.
+    cnt = 0
+    for edge in edges:
+        (lat1, long1), (lat2, long2) = edge
+        # The point must be between the values of the longitude of the edge (Point pair)
+        cond1 = (long < long1) != (long < long2)
+        # The point must be:
+        # - The latitude value must be lower than both latitude values of the edge to be inside  # noqa: E501
+        # - The latitude value must bigger than both latitude values of the edge to be outside  # noqa: E501
+        # - If it is between the both latitude values of the edge:
+        #   Consider lat_zero the latitude value of the exact point where the ray crosses the edge line  # noqa: E501
+        #   - It is inside if the lat is before lat_zero
+        #   - It is outside if the lat is before lat_zero
+        #
+        # The calculation of the lat_zero and check the validity can be simplified by
+        # the equation used below
+        cond2 = lat < lat1 + ((long - long1) / (long2 - long1)) * (lat2 - lat1)
+
+        if cond1 and cond2:
+            cnt += 1
+
+    return cnt % 2 == 1
+
+
 def apply_contour(contour_df: pd.DataFrame, data_df: pd.DataFrame) -> pd.DataFrame:
     pass
 
